579e935fcc53df6244b0fbc89142e40a87b3ee0c
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-05-26 13:33:52 -0300

    redirected to log in page if not logged in

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,12 +2,17 @@ import React, {useEffect, useState} from "react";
 import {BASE_URL, PORT} from "../utils/constants";
 import axios from 'axios';
 import BikeInfo from "../components/BikeInfo";
+import {useNavigate} from "react-router-dom";
 
 const HomePage = () => {
-
     const [bikes, setBikes] = useState([]);
 
+    const navigate = useNavigate();
+
     useEffect(() => {
+        if (!localStorage.getItem("email")) {
+            navigate('/login');
+        }
         axios.get(BASE_URL + ':' + PORT + "/bikes").then(res => setBikes(res.data)).catch(e => console.log(e))
     }, []);
 
