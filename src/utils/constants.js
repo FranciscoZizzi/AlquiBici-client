e5c7ef70e473a286c4cbe7727295670bf07552f9
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-05-31 20:15:18 -0300

    modified mqtt constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const SERVER_HOSTNAME = '18.232.136.33'
 export const SERVER_PORT = '3002'
 
-export const MQTT_HOSTNAME = 'localhost'
+export const MQTT_HOSTNAME = '0.0.0.0'
 export const MQTT_PORT = '1883'
