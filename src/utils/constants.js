1daaa4a3257d548b8d684d0fa70f7d9f749b4c95
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-06-01 13:02:16 -0300

    testing mqtt

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const SERVER_HOSTNAME = '18.232.136.33'
 export const SERVER_PORT = '3002'
 
-export const MQTT_HOSTNAME = '3.95.218.205'
+export const MQTT_HOSTNAME = '54.204.142.108'
 export const MQTT_PORT = '9850'
