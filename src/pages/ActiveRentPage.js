ed3cacbb858da173970dad5ccdfbd55f72457861
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-06-01 13:01:37 -0300

    testing mqtt

--- src/pages/ActiveRentPage.js
+++ src/pages/ActiveRentPage.js
@@ -1,17 +1,23 @@
 import React, {useEffect, useState} from 'react';
 import BikeMap from "../components/BikeMap";
-import {json} from "react-router-dom";
 
-const ActiveRentPage = ({client}) => {
-    const [data, setPosition] = useState(
+const ActiveRentPage = ({client, bikeId}) => {
+    const [positionData, setPositionData] = useState(
         {
-            coords: [30.2675, -0.7409],
+            coords: [0, 0],
             name: "Current position",
         },
     );
 
     useEffect(() => {
-        client.subscribe("test", (e) => {
+        client.subscribe("position", (e) => {
+            if (!e) {
+                console.log("subscribed successfully");
+            } else {
+                console.log(e);
+            }
+        });
+        client.subscribe("distance", (e) => {
             if (!e) {
                 console.log("subscribed successfully");
             } else {
@@ -19,24 +25,45 @@ const ActiveRentPage = ({client}) => {
             }
         });
         client.on("message", (topic, message) => {
-            if (topic === "test") {
-                let jsonString = Array.from(message).map(byte => String.fromCharCode(byte)).join('');
-                console.log(jsonString)
-                let json = JSON.parse(jsonString);
-                setPosition({coords: [json.lat, json.long], name: "Current position"})
+            if (topic === "position") { // TODO change to actual topic
+                let json = toJson(message);
+                setPosition(json.lat, json.long);
+            }
+            if (topic === "distance") { // TODO change to actual topic
+                let distance = toInt(message);
+                console.log(message);
+                console.log(distance);
             }
         })
     }, []);
 
     const handleClick = () => {
-        setPosition({coords: [0, 0], name: "Current position"})
+        setPosition(0, 0);
+    }
+
+    const setPosition = (lat, long) => {
+        setPositionData({coords: [0, 0], name: "Current position"});
+    }
+
+    const toJson = (byteArray) => {
+        let jsonString = Array.from(byteArray).map(byte => String.fromCharCode(byte)).join('');
+        console.log(jsonString)
+        return JSON.parse(jsonString);
+    }
+
+    const toInt = (byteArray) => {
+        let value = 0;
+        for (let i = 0; i < byteArray.length; i++) {
+            value = (value << 8) | byteArray[i];
+        }
+        return value;
     }
 
     return (
         <div>
             <button onClick={handleClick}>press me</button>
             <div style={{width: "50%"}}>
-                <BikeMap data={[data]}/>
+                <BikeMap data={[positionData]}/>
             </div>
         </div>
     );
