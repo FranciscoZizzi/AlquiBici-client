8bcdfa22bb6b1346a05aba3abe173ae373bb9111
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-06-03 17:39:33 -0300

    fixed funds adding instead of subtracting

--- src/components/BikeInfo.js
+++ src/components/BikeInfo.js
@@ -49,7 +49,7 @@ const BikeInfo = ({bike, client}) => {
                 const modifyBalance = async () => {
                     let bikeRes = await axios.get('http://' + SERVER_HOSTNAME + ':' + SERVER_PORT + `/bikes/get/${bike.id}`).catch((e) => console.log("error"));
                     let price = bikeRes.data.price
-                    let funds = json.distance * price / 1000;
+                    let funds = -json.distance * price / 1000;
                     await axios.post('http://' + SERVER_HOSTNAME + ':' + SERVER_PORT + '/users/add-funds', {email: localStorage.getItem("email"), funds: funds});
                 }
                 modifyBalance();
