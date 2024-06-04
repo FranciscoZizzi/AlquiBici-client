92b2fff16f49e52ff9a44d2aadb7017ff2ed6605
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-06-04 11:31:07 -0300

    :(

--- src/components/BikeInfo.js
+++ src/components/BikeInfo.js
@@ -21,7 +21,7 @@ const BikeInfo = ({bike, client}) => {
         let email = localStorage.getItem("email");
         axios.post('http://' + SERVER_HOSTNAME + ':' + SERVER_PORT + '/bikes/rent', {email: email, bikeId: bike.id}).then(res => {
             setUpdate(!update);
-            axios.post("http://" + SERVER_HOSTNAME + ":" + SERVER_PORT + `/users/get/${email}`).then(res => {
+            axios.get("http://" + SERVER_HOSTNAME + ":" + SERVER_PORT + `/users/get/${email}`).then(res => {
                 let balance = res.data.balance;
                 let price = bike.price;
                 client.publish('alquibici/' + bike.id + '/rent-status', `rent ${1000 * balance/price}`);
