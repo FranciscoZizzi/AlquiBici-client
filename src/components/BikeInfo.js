c8454a9b2541eed392fb5490da498315eec0eaee
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-06-03 21:28:30 -0300

    max distance with available balance sent to ESP32

--- src/components/BikeInfo.js
+++ src/components/BikeInfo.js
@@ -10,7 +10,6 @@ const BikeInfo = ({bike, client}) => {
     const navigate = useNavigate();
 
     useEffect(() => {
-
         const getBikeData = async () => {
             let res = await axios.get('http://' + SERVER_HOSTNAME + ':' + SERVER_PORT + `/bikes/get/${bike.id}`);
             setBikeInfo(res.data);
@@ -22,8 +21,12 @@ const BikeInfo = ({bike, client}) => {
         let email = localStorage.getItem("email");
         axios.post('http://' + SERVER_HOSTNAME + ':' + SERVER_PORT + '/bikes/rent', {email: email, bikeId: bike.id}).then(res => {
             setUpdate(!update);
-            client.publish('alquibici/' + bike.id + '/rent-status', 'rent');
-            alert(res.data.message);
+            axios.post("http://" + SERVER_HOSTNAME + ":" + SERVER_PORT + `/users/get/${email}`).then(res => {
+                let balance = res.data.balance;
+                let price = bike.price;
+                client.publish('alquibici/' + bike.id + '/rent-status', `rent ${1000 * balance/price}`);
+                alert(res.data.message);
+            })
         }).catch(e => {
             setUpdate(!update);
             alert(e.response.data.message);
